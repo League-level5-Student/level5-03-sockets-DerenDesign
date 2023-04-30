b89ca48289b1d291f6df164ff8b38791db8926c1
DerenDesign <67876073+DerenDesign@users.noreply.github.com>
2023-04-30 12:33:55 -0700

    Done

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -10,20 +10,27 @@ public class ClientGreeter {
 	  // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
       String ip = "70.95.176.211";
       //2. Create an integer for the server's port number
-      int serverPort = 0;
+      int serverPort = 8080;
       //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
-    
+    try {
+		
+	
     	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
- 
+    	Socket socket = new Socket(ip, serverPort);
          //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
-         
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
          //6. Use the DataOutputStream object to send a message to the server using the writeUTF(String message) method
-         
+         output.writeUTF("Hi there!");
          //7. Create a DataInputStream object. When initializing it, use the Server object you created in step 4 to call the getInputStream() method.
-         
+         DataInputStream input = new DataInputStream(socket.getInputStream());
          //8. Use the DataInputStream object to print a message from the server using the readUTF() method.
-         
+         System.out.println(input.readUTF());
          //9. Close the client's server object
+         socket.close();
+    } catch (IOException e) {
+		// TODO: handle exception
+    	System.out.println(e.getStackTrace());
+	}
 
    }
 }
--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,9 +1,117 @@
 package _02_Chat_Application;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.swing.JOptionPane;
+
 /*
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
 
 public class ChatApp {
+	boolean working = true;
+	public static void main(String[] args) {
+			ChatApp app = new ChatApp();
+			app.run();
+	}
+	
+	public void run() {
+		String setting = JOptionPane.showInputDialog("Are you a client or server (c or s)");
+		if(setting.equalsIgnoreCase("c")) {
+			client();
+		}
+		else {
+			server();
+		}
+	}
+	
+	public void client() {
+		System.out.println("Client");
+		String ip = JOptionPane.showInputDialog("Server IP");
+		int port = Integer.parseInt(JOptionPane.showInputDialog("Port #"));
+		
+		System.out.println("Connected!");
+		
+		try {
+			Socket socket = new Socket(ip, port);
+			
+			Thread writer = new Thread(() -> write(socket, "Client"));
+			
+			Thread reader = new Thread(() -> recieve(socket));
+			
+			writer.start();
+			reader.start();
+		} catch (IOException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	
+	public void server() {
+		try {
+		System.out.println("Server");
+		ServerSocket server = new ServerSocket(8080);
+		
+		System.out.println("Connecting...");
+		
+		Socket socket = server.accept();
+		
+		System.out.println("Connected");
+		
+		Thread writer = new Thread(() -> write(socket, "Server"));
+			
+		Thread reader = new Thread(() -> recieve(socket));
+			
+			writer.start();
+			reader.start();
+		while(working) {
+			try {
+				Thread.sleep(10);
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		server.close();
+		System.exit(0);
+		
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+		
+	}
+	
+	public void write(Socket socket, String thing) {
+		while(working) {
+			try {
+				DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+				String message = JOptionPane.showInputDialog(thing + " message");
+				output.writeUTF(message);
+				System.out.println("You responded with " + message);
+			} catch (IOException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	public void recieve(Socket socket) {
+		while(working) {
+			try {
+				DataInputStream input = new DataInputStream(socket.getInputStream());
+				String message = input.readUTF();
+				System.out.println("They responded with " + message);
+			} catch (IOException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	}
 
-}
